edb3796b007c5c7d94e3c9abd1fb8d6dafb5cb66
Luciana Busquets <lubusquets@hotmail.com>
2026-05-28 23:15:50 -0300

    Clínica de Saúde - 1 bloco

---
+++ arquivo.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+arquivo = 'pacientes.json'
+
+print(' Sistema de Cadastro de Pacientes '.center(50, '-'))
+print()
+
+#carregar dados do arquivo
+def carregar_dados():
+    if not os.path.exists(arquivo):
+        return []
+    try:
+        with open(arquivo, 'r', encoding='utf-8') as f:
+            dados = json.load(f) #validação simples.
+            if isinstance(dados, list):
+                return dados
+            else:
+                print('⚠Formato do arquivo inválido. Iniciando com lista vazia.')
+                return []
+    except (json.JSONDecodeError, IOError) as e:
+        print(f"⚠Erro ao carregar dados '{arquivo}': {e}. Iniciando com lista vazia.")
+        return []
+    
